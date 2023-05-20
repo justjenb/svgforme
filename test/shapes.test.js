5550eb867f919b194222f2fe7ac969e36be15afe
Jen B <jenbutler984@gmail.com>
2023-05-19 22:20:45 -0400

    working on circle svg generation, text gets added but textcolor/shapecolor come out undefined still

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,3 +1,5 @@
+const Triangle = require("../lib/triangle");
+
 const shape = new Triangle();
 shape.setColor("blue");
 expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
